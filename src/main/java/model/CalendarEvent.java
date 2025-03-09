2871652f32268d5223971e2056e4797b984a1b22
saswata0502 <57698542+saswata0502@users.noreply.github.com>
2025-03-09 01:45:06 -0500

    Update CalendarEvent.java

--- src/main/java/model/CalendarEvent.java
+++ src/main/java/model/CalendarEvent.java
@@ -1,22 +1,32 @@
 package model;
 
-
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.List;
 
-class CalendarEvent {
+public class CalendarEvent {
   private String eventName;
   private LocalDateTime startDateTime;
   private LocalDateTime endDateTime;
-  private boolean isAutoDecline;
+  private String description;
+  private String location;
+  private boolean isPublic;
+  private boolean isRecurring;
+  private List<DayOfWeek> recurrenceDays;
+  private boolean autoDecline;
 
   public CalendarEvent(String eventName, LocalDateTime startDateTime, LocalDateTime endDateTime,
-                       boolean autoDecline) {
+                       String description, String location, boolean isPublic, boolean isRecurring,
+                       List<DayOfWeek> recurrenceDays, boolean autoDecline) {
     this.eventName = eventName;
     this.startDateTime = startDateTime;
     this.endDateTime = endDateTime;
-    this.isAutoDecline = autoDecline;
+    this.description = description;
+    this.location = location;
+    this.isPublic = isPublic;
+    this.isRecurring = isRecurring;
+    this.recurrenceDays = recurrenceDays;
+    this.autoDecline = autoDecline;
   }
 
   public String getEventName() {
@@ -29,5 +39,50 @@ class CalendarEvent {
 
   public LocalDateTime getEndDateTime() {
     return endDateTime;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public boolean isPublic() {
+    return isPublic;
+  }
+
+  public boolean isRecurring() {
+    return isRecurring;
+  }
+
+  public List<DayOfWeek> getRecurrenceDays() {
+    return recurrenceDays;
+  }
+
+  public boolean isAutoDecline() {
+    return autoDecline;
+  }
+
+  // Setters for editable fields
+  public void setEventName(String eventName) {
+    this.eventName = eventName;
+  }
+
+  public void setStartDateTime(LocalDateTime startDateTime) {
+    this.startDateTime = startDateTime;
+  }
+
+  public void setEndDateTime(LocalDateTime endDateTime) {
+    this.endDateTime = endDateTime;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
   }
 }
